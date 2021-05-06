1910a9c1b1f8c432c26fcab26e2245c758d14677
José Siqueira <47609042+josesiqueira@users.noreply.github.com>
2021-05-05 23:01:06 -0300

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,171 @@
+const filter = document.querySelector("#filterCards");
+const filterContainer = document.querySelector(".container__filters");
+const buttonResult = document.querySelector("#result");
+const titlePage = document.querySelector("#game__title");
+const startAgain = document.querySelector("#start");
+const container = document.querySelector('.container__options');
+const alert = document.querySelector('.alert')
+
+// Função que seleciona os cards e ativa o botão de resultado
+const selectedCards = () => {
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card) => {
+    card.addEventListener("click", function () {
+      this.classList.toggle("active");
+    });
+  });
+};
+
+// Função para mostrar as cartas selecionadas
+const statePageResult = () => {
+  buttonResult.style.display = "none";
+  filterContainer.style.display = "none";
+
+  startAgain.style.display = "block"
+  container.style.display = "flex";
+  container.style.justifyContent="center";
+
+  titlePage.innerText = "Result";
+  alert.style.display = "none";
+};
+
+// Função para selecionar as cartas
+const statePageSelect = () => {
+  buttonResult.style.display = "block";
+  titlePage.innerText = "Select cards";
+
+  startAgain.style.display = "none";
+
+  filterContainer.style.display = "flex";
+  container.style.display = "flex";
+  container.style.justifyContent="space-between";
+  alert.style.display = 'none'
+};
+
+// Função que faz os filtros dos cards
+filter.addEventListener("click", (event) => {
+  if (event.target.value === "all") {
+    return createCards(infosCards);
+  }
+
+  statePageSelect();
+
+  const cards = filterCards(infosCards, event.target.value);
+  return createCards(cards);
+});
+
+// Função que filtra o array
+const filterCards = (array, type) => {
+  return array.filter((infos) => {
+    return infos.type === type;
+  });
+};
+
+// Função que formata a classes de CSS
+const formatClasse = (type) => {
+  return type.toLowerCase();
+};
+
+//  Template de List do Card
+const templateList = (list) => {
+  return list.reduce((accumulator, actual) => {
+    return (accumulator += `
+      <li>${actual}</li>
+     `);
+  }, "");
+};
+
+const templateListLink = (list) => {
+  return list.reduce((accumulator, actual) => {
+    return (accumulator += `
+      <li>
+      <a class="link" href="${actual.link}" target="_blank">${actual.name}</a>
+      </li>
+     `);
+  }, "");
+};
+
+
+// Função que pega template criado e adiciona no DOM
+const insertCardsIntoPage = (template) => {
+  const cards = document.querySelector(".cards");
+  cards.innerHTML = template;
+  // Adiciona funcionalidade de selecionar os cards
+  selectedCards();
+};
+
+// Função para a criação de cada um dos cards 
+const createCards = (infos) => {
+  const template = infos.reduce((accumulator, actual) => {
+    return (accumulator += `
+    <article class="card">
+    <div class="card__header ${formatClasse(actual.type)}">
+      <p class="card__number">#${actual.number}</p>
+      <h3 class="card__title">${actual.title}</h3>
+      <span class="card__badge">${actual.type}</span>
+    </div>
+    <div class="card__body">
+      <p>
+        <span class="strong">Preamble: </span>${actual.preamble}
+      </p>
+      <br>
+      <span class="strong">Issues to be addressed:</span> 
+      <ul class="issues__list">
+        ${templateList(actual.issues)}
+      </ul>
+      <br>
+      <p>
+        ${
+          actual.practicalExample
+            ? `<span class="strong">Illustration:</span> ${actual.practicalExample} `
+            : ""
+        }
+      </p>
+      <p>
+      ${
+        actual.tools
+          ? `<p><strong>Tool Suggestion:</strong></p> 
+             <ul class="tools__list">
+             ${templateListLink(actual.tools)}
+             </ul>
+            `
+          : ""
+      }
+    </p>
+ 
+    </div>
+  </article>
+    `);
+  }, "");
+  insertCardsIntoPage(template);
+};
+
+// Função que captura todos cards que estão ativos e transforma em um array.
+buttonResult.addEventListener("click", () => {
+  const selectedCards = document.querySelectorAll(".active");
+  const cards = document.querySelector(".cards");
+
+  // Definir o mínimo de cartas a ser selecionado pelo guia
+  if (selectedCards.length >= 2) {
+    cards.innerHTML = "";
+    selectedCards.forEach((card) => {
+      card.classList.remove("active");
+      cards.appendChild(card);
+    });
+    statePageResult();
+    return
+  } 
+  return alert.style.display = 'block'
+
+});
+
+startAgain.addEventListener("click", () => {
+  statePageSelect();
+  createCards(infosCards);
+});
+
+const init = () => {
+  createCards(infosCards);
+};
+
+init();
